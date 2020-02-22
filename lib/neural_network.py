1eaf3513015f7db4817026d4d3aa8f884219b9ce
Erwin Lejeune <erwin.lejeune15@gmail.com>
2020-02-22 02:00:56 +0100

    🎉 Build Neural Network object

--- lib/neural_network.py
+++ lib/neural_network.py
@@ -1,6 +1,26 @@
 import numpy as np 
 
 class NeuralNetwork():
-    pass
+    def __init__(self, layers, batch_size, epochs, learning_rate):
+        self.layers = layers
+        self.batch_size = batch_size
+        self.epochs = epochs
+        self.learning_rate = learning_rate
 
+    def forward_propagate(self, inputs):
+        pass
 
+    def loss_function(self, inputs):
+        pass
+
+    def back_propagate(self, output, target):
+        pass
+
+    def gradiant_descent(self):
+        pass
+
+    def fit(self, inputs, outputs):
+        pass
+
+    def test(self, inputs, outputs):
+        pass
