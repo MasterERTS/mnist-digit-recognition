242e3d4f50b7cb90e4a287b102acdb0456aad23c
Erwin Lejeune <erwin.lejeune15@gmail.com>
2020-02-22 01:49:58 +0100

    🚧 wip : mnist recognition

--- src/mnist_rec.py
+++ src/mnist_rec.py
@@ -12,20 +12,22 @@ from mnist import MNIST
 # https://stackoverflow.com/questions/44115411/mnist-training-error-in-for-loop
 # https://github.com/louisjc/mnist-neural-network
 
+output_layer = 10
+
 mndata = MNIST('../res/')
 mndata.gz = True
-images, labels = mndata.load_training()
+training_images, training_labels = mndata.load_training()
+testing_images, testing_labels = mndata.load_testing()
 
-train_inputs = [np.reshape(x, (784, 1)) for x in images[0]]
-train_results = [vectorized_result(y) for y in images[1]]
-val_inputs = [np.reshape(x, (784, 1)) for x in labels[0]]
-val_results = labels[1]
+training_inputs = training_images.reshape(training_images.shape[0], training_images.shape[1] * training_images.shape[2]).astype('float32')
+normalized_inputs = training_inputs/255
+normalized_outputs = np.eye(output_layer)[training_labels]
+
+testing_inputs = testing_images.reshape(testing_images.shape[0], testing_images.shape[1]*testing_images.shape[2]).astype('float32')
+norm_test_inputs= testing_inputs/255
+norm_test_outputs = testing_labels
 
 layers = [784, 30, 10]
 learning_rate = 0.05
-
-w1 = np.random.rand(784, 1)
-w2 = 0
-
-a0 = images[0]
-a1 = sigmoid(w1*a0)
+batch_size = 1
+epochs = 100
