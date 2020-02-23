e4a450cc04ee8790e5f6f7df2f8ea31169cca18f
Erwin Lejeune <erwin.lejeune15@gmail.com>
2020-02-23 16:24:25 +0100

    🐛 fix merge issue

--- lib/neural_network.py
+++ lib/neural_network.py
@@ -1,6 +1,81 @@
-import numpy as np 
+import numpy as np
+import lib.maths_util as mathlib
+
 
 class NeuralNetwork():
-    pass
+    def __init__(self, layers, batch_size, epochs, learning_rate):
+        self.layers = layers
+        self.batch_size = batch_size
+        self.epochs = epochs
+        self.learning_rate = learning_rate
 
+        self.weights = []
+        self.biases = []
+        self.loss = []
+        n_layers = len(layers)
 
+        for i in range(n_layers - 1):
+            self.weights.append(np.random.normal(
+                0, 1, [self.layers[i], self.layers[i+1]]))
+            self.biases.append(np.zeros((1, self.layers[i+1])))
+
+    def feed_forward(self, inputs):
+
+        layer0 = inputs
+        layer1 = mathlib.relu(np.dot(layer0, self.weights[0]) + self.biases[0])
+        layer2 = mathlib.softmax(
+            np.dot(layer1, self.weights[1]) + self.biases[1])
+
+        return layer1, layer2
+
+    def loss_function(self, predicted_outputs, outputs):
+        loss = mathlib.cross_entropy(predicted_outputs, outputs)
+        loss += mathlib.regularization(0.01, self.weights[0], self.weights[1])
+
+        return loss
+
+    def back_propagate(self, inputs, hidden_layer, predicted_outputs, outputs):
+        delta_y = (predicted_outputs - outputs) / predicted_outputs.shape[0]
+        delta_hidden_layer = np.dot(delta_y, self.weights[1].T)
+        delta_hidden_layer[hidden_layer <= 0] = 0
+
+        w2_grad = np.dot(hidden_layer.T, delta_y)
+        b2_grad = np.sum(delta_y, axis=0, keepdims=True)
+
+        w1_grad = np.dot(inputs.T, delta_hidden_layer)
+        b1_grad = np.sum(delta_hidden_layer, axis=0, keepdims=True)
+
+        w2_grad += 0.01 * self.weights[1]
+        w1_grad += 0.01 * self.weights[0]
+
+        self.weights[0] -= self.learning_rate * w1_grad
+        self.biases[0] -= self.learning_rate * b1_grad
+
+        self.weights[1] -= self.learning_rate * w2_grad
+        self.biases[1] -= self.learning_rate * b2_grad
+
+    def fit(self, inputs, outputs):
+        for epoch in range(self.epochs):
+            it = 0
+            while it < len(inputs):
+                inputs_batch = inputs[it:it+self.batch_size]
+                outputs_batch = outputs[it:it+self.batch_size]
+                hidden_layer, output_layer = self.feed_forward(inputs_batch)
+                loss = self.loss_function(output_layer, outputs_batch)
+                self.loss.append(loss)
+                self.back_propagate(inputs_batch, hidden_layer,
+                                    output_layer, outputs_batch)
+
+                '''print('=== Epoch: {:d}/{:d}\tIteration:{:d}\tLoss: {:.2f} ==='.format(
+                    epoch+1, self.epochs, it+1, loss))'''
+                it += self.batch_size
+
+    def predict(self, inputs):
+        hidden_layer, prediction = self.feed_forward(inputs)
+        return prediction
+
+    def accuracy_test(self, inputs, result):
+        prediction = self.predict(inputs)
+        acc = float(np.sum(np.argmax(prediction, 1) == result)) / \
+            float(len(result))
+        print('Test accuracy : {:.2f}%'.format(acc*100))
