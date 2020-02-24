53ffd871a890802a92fc0f0dd92a17354aeec466
Erwin Lejeune <erwin.lejeune15@gmail.com>
2020-02-24 18:08:49 +0100

    ✅ add two accuracy tests

--- tests/test_or.py
+++ tests/test_or.py
@@ -1,27 +1,69 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
 import numpy as np
+import math
+import random
+import matplotlib.pyplot as plt
 import sys
 from os import path
 sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 
+import mnist_parser as mnist
 from lib.neural_network import NeuralNetwork
 
-def test_or_gate():
-    expected_inputs = np.array([[0, 0],
-                         [0, 1],
-                         [1, 0],
-                         [1, 1]])
+def test_high_acc():
+    output_layer = 10
 
-    expected_outputs = np.array([[0, 1, 1, 1]]).T
+    training_images = mnist.train_images()
+    training_labels = mnist.train_labels()
+    testing_images = mnist.test_images()
+    testing_labels = mnist.test_labels()
 
-    layers = [2, 1, 1]
-    epochs = 5
+    training_inputs = training_images.reshape(
+        training_images.shape[0], training_images.shape[1] * training_images.shape[2]).astype('float32')
+    normalized_inputs = training_inputs/255
+    normalized_outputs = np.eye(output_layer)[training_labels]
+
+    testing_inputs = testing_images.reshape(
+        testing_images.shape[0], testing_images.shape[1]*testing_images.shape[2]).astype('float32')
+    norm_test_inputs = testing_inputs/255
+    norm_test_outputs = testing_labels
+
+    layers = [784, 30, 10]
+
+    learning_rate = 0.001
     batch_size = 1
-    learning_rate = 0.1
+    epochs = 5
 
     nn = NeuralNetwork(layers, batch_size, epochs, learning_rate)
+    nn.fit(normalized_inputs, normalized_outputs, False)
+    acc = nn.accuracy_test(norm_test_inputs, norm_test_outputs)
+    assert(acc > 0.9)
 
-    nn.fit(expected_inputs, expected_outputs)
 
+def test_low_acc():
+    output_layer = 10
+
+    training_images = mnist.train_images()
+    training_labels = mnist.train_labels()
+    testing_images = mnist.test_images()
+    testing_labels = mnist.test_labels()
+
+    training_inputs = training_images.reshape(
+        training_images.shape[0], training_images.shape[1] * training_images.shape[2]).astype('float32')
+    normalized_inputs = training_inputs/255
+    normalized_outputs = np.eye(output_layer)[training_labels]
+
+    testing_inputs = testing_images.reshape(
+        testing_images.shape[0], testing_images.shape[1]*testing_images.shape[2]).astype('float32')
+    norm_test_inputs = testing_inputs/255
+    norm_test_outputs = testing_labels
+
+    layers = [784, 30, 10]
+
+    learning_rate = 0.1
+    batch_size = 1
+    epochs = 10
+
+    nn = NeuralNetwork(layers, batch_size, epochs, learning_rate)
+    nn.fit(normalized_inputs, normalized_outputs, False)
+    acc = nn.accuracy_test(norm_test_inputs, norm_test_outputs)
+    assert(acc > 0.6)
